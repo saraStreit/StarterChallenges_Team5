17565f59db212de35ba02b070995f4e8327fedd6
saraStreit <sara.streit@ag.ch>
2024-10-18 23:07:11 +0200

    feat: jaja

---
+++ Backend/MisBackend/DTOs/UpdateUserDto.cs
@@ -0,0 +1,8 @@
+namespace Backend.DTOs;
+
+public class UpdateUserDto
+{
+    public string? Username { get; set; }
+    public string? Email { get; set; }
+    public string? Password { get; set; }
+}
---
+++ Backend/MisBackend/Program.cs
@@ -0,0 +1,45 @@
+using Backend.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(
+        policyBuilder =>
+        {
+            var origins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>() ?? [];
+            policyBuilder.WithOrigins(origins)
+                .SetIsOriginAllowedToAllowWildcardSubdomains()
+                .SetPreflightMaxAge(TimeSpan.FromHours(24));
+        });
+});
+
+var app = builder.Build();
+
+app.UseCors();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
