6cd1ca3e025267c6b4d54f36a84635aa10a93fc1
Pasha-esch-suess <selina@fredic.ch>
2024-10-18 19:05:01 +0200

    wip

---
+++ frontend/src/components/statsblock/body.tsx
@@ -0,0 +1,17 @@
+const WisdomDisplay = ( ) => {
+
+    return (
+  <div className="container">
+  <div className="wisdom-box">
+  <p>Wisdom</p>
+  {/* <div className="value">{wisdomValue}</div>
+  <div className="small-box">{smallValue}</div> */}
+  </div>
+  </div>
+  
+    );
+  
+  };
+  
+  export default WisdomDisplay;
+   
