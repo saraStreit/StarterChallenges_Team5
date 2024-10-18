f3de73a3051d02e6b29478f1fd38d73a4a06ff5a
saraStreit <sara.streit@ag.ch>
2024-10-19 01:41:19 +0200

    feat: ja weiss ichhh?

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,54 @@
-
-import './App.scss'
+import './App.scss';
 import Header from "./components/header/Header.tsx";
 import CharacterOverview from "./components/characterOverview/CharacterOverview.tsx";
-import {Character} from "./components/characterOverview/Character.ts";
-import {backendUrl} from "./constants.tsx";
+import { Character } from "./components/characterOverview/Character.ts";
+import { backendUrl } from "./constants.tsx";
 import BiepBupBar from "./components/actionBar/biepBupbar.tsx";
-import {useEffect, useState} from "react";
+import { useEffect, useState, useRef } from "react";
 
- function App() {
+function App() {
+    const [characters, setCharacters] = useState<Character[]>();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
-     const [characters, setCharacters] = useState<Character[]>();
-     const [loading, setLoading] = useState(true);
-     const [error, setError] = useState(null);
+    useEffect(() => {
+        const fetchCharacters = async () => {
+            try {
+                const response = await fetch(backendUrl);
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                const result = await response.json();
+                setCharacters(result);
+            } catch (error) {
+                // setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+        fetchCharacters();
+    }, []);
 
-     useEffect(() => {
-         const fetchCharacters = async () => {
-             try {
-                 const response = await fetch(backendUrl);
-                 if (!response.ok) {
-                     throw new Error('Network response was not ok');
-                 }
-                 const result = await response.json();
-                 setCharacters(result);
-             } catch (error) {
-                 setError(error.message);
-             } finally {
-                 setLoading(false);
-             }
-         };
-         fetchCharacters();
-     }, []);
 
-     if (loading) return <div>Loading...</div>;
-     if (error) return <div>Error: {error}</div>;
-  return (
-    <>
-      <div className={"wrapper"}>
-        <Header />
-          <BiepBupBar />
-          <div className={"content"}>
-              <h1>Character Overview</h1>
-              { characters && <CharacterOverview characters={characters}/> }
-          </div>
-      </div>
-    </>
-  )
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error}</div>;
+
+    return (
+        <>
+            <div className={"wrapper"}>
+                <Header />
+                <div className={"content"}>
+                    <div className={"startOfContent"}>
+                        <h1>My Characters</h1>
+                        <BiepBupBar />
+                    </div>
+                    <div className={"centeredContent"}>
+                        {characters && <CharacterOverview characters={characters} />}
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
