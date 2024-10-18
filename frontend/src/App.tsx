53d5cbc0ced1ec74cc9a7134f86bb26df51c6635
saraStreit <sara.streit@ag.ch>
2024-10-18 16:01:17 +0200

    feat(frontend): add header

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 
 import './App.scss'
+import Header from "./components/header/Header.tsx";
 
 function App() {
 
   return (
     <>
       <div className={"wrapper"}>
-
+        <Header />
       </div>
     </>
   )
