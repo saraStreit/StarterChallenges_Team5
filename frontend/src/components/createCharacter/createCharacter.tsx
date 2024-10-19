0f2a06108af225ac79ba6b65e52d476e4579f249
saraStreit <sara.streit@ag.ch>
2024-10-19 04:36:59 +0200

    feat: pretty basic dashboard

--- frontend/src/components/createCharacter/createCharacter.tsx
+++ frontend/src/components/createCharacter/createCharacter.tsx
@@ -31,6 +31,7 @@
 
 import PrimaryButton from "../basicComponents/button.tsx";
 import {Character} from "../characterOverview/Character.ts";
+import {useState} from "react";
 
 interface createCharacterDialogProps {
     isOpen: boolean;
@@ -38,14 +39,38 @@ interface createCharacterDialogProps {
     createCharacter: (character : Character) => void;
 }
 const CreateCharacterDialog = ({ isOpen, onClose, createCharacter }: createCharacterDialogProps) => {
+    const [inputValue, setInputValue] = useState('');
     if (!isOpen) return null;
+    const handleInputChange = (event) => {
+        if (event.target.value)
+        {
+            setInputValue(event.target.value);
+        }
+    }
+
 
     return (
         <div className="dialog-overlay">
             <div className="dialog-content">
                 <button className="dialog-close" onClick={onClose}>X</button>
-                <div>some content</div>
-                <PrimaryButton text={"createCharacter"} onClick={() => createCharacter({name: "biebediBabedi"})}/>
+                <div className={"dialog-content-content"}>
+                <div className="dialog-title">Create Character</div>
+                    <div className={"dialog-input"}>
+                <div>Name:</div>  <input className={"dialog-input-box"} type="text" onChange={handleInputChange} value={inputValue} defaultValue={"Helga"}/>
+                    </div>
+                <select className={"classSelect"}>
+                    <option value="1">Barbarian</option>
+                    <option value="2">Mage</option>
+                    <option value="3">Rouge</option>
+                    <option value="4">Warrior</option>
+                    <option value="5">Paladin</option>
+                    <option value="6">Priest</option>
+                    <option value="7">Druid</option>
+                    <option value="8">Bard</option>
+                </select>
+                </div>
+
+                <PrimaryButton text={"createCharacter"} onClick={() => createCharacter({name: inputValue})}/>
             </div>
         </div>
     );
